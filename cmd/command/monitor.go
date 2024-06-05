1f7d9ad6aab8d7831b6c3221cdc2c5ef34ecf6f8
maxipaz <pazmaxi1997@gmail.com>
2024-06-05 19:54:41 +0200

    WIP

---
+++ cmd/command/monitor.go
@@ -0,0 +1,37 @@
+package command
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/maxipaz/wallet/config"
+	"github.com/maxipaz/wallet/pkg/blockchain"
+	"github.com/spf13/cobra"
+)
+
+// NewMonitorCommand creates the monitor command
+func NewMonitorCommand(ctx context.Context) *cobra.Command {
+	monitorCommand := &cobra.Command{
+		Use:   "monitor",
+		Short: "Monitor events in the blockchain",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return monitoring(ctx)
+		},
+	}
+	monitorCommand.Flags().StringP("contract.address", "c", "", "Contract address")
+	return monitorCommand
+}
+
+func monitoring(ctx context.Context) error {
+	ctxCall, cancel := context.WithTimeout(ctx, config.App.Blockchain.TimeoutIn)
+	defer cancel()
+	client, err := ethclient.DialContext(ctxCall, config.App.Blockchain.WS)
+	if err != nil {
+		return err
+	}
+	monitor := blockchain.NewMonitor(config.App.Contract.Address)
+	err = monitor.Start(ctx, client)
+	if err != nil {
+		return err
+	}
+	return nil
+}
